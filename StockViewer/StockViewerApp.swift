6235659350370f227cdee36bfd84b7981dcbfc03
Alpay Ozer <alpay.ozer@student.hogent.be>
2024-10-28 14:19:08 +0100

    Initial Commit

---
+++ StockViewer/StockViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockViewerApp.swift
+//  StockViewer
+//
+//  Created by Alpay Ozer on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct StockViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StockViewerTests/StockViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  StockViewerTests.swift
+//  StockViewerTests
+//
+//  Created by Alpay Ozer on 28/10/2024.
+//
+
+import Testing
+@testable import StockViewer
+
+struct StockViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
